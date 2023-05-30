543497a755d3a4ce765fba3e6bad5c2b2631dbc1
callumforrester <callumforrester@users.noreply.github.com>
2023-05-30 13:13:46 +0000

    deploy: 696735484150cf20c53322e853154ac7fe8afd8a

--- helm-chart/_static/documentation_options.js
+++ helm-chart/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.4.dev1+gae2b9af',
+    VERSION: '0.1.4.dev2+g6967354',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
